04de9a61922744c362285b5bc4a2054c651748ed
Kiara Johnson <johns.kia35@gmail.com>
2022-11-08 14:56:36 -0700

    main base

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
-// class MyShip {
-//   constructor(name, hull, firepower, accuracy) {
-//   name = "USS Medusa";
-//   hull = 20;
-//   firepower = 5;
-//   accuracy = 0.7;
-//   }
-// attack(){
-//     if (Math.random() < this.accuracy) {
-//       console.log("Enemy ship has received 5 damage.");
-//     } else {
-//         console.log('Attack missed');
-//     }
-//     console.log(Math.random(), this.accuracy)
-//   }
-// };
 const myShip = {
-    name: "USS Medusa",
-    hull: 20,
-    firepower: 5,
-    accuracy: 0.7,
-  
-    attack: () => {
-      if (Math.random() < myShip.accuracy) {
-        console.log("Enemy ship has received 5 damage.");
-      } else {
-          console.log('Attack missed');
-      }
-      console.log(Math.random(), myShip.accuracy)
-    },
-  };
-  
+  name: "USS Medusa",
+  hull: 20,
+  firepower: 5,
+  accuracy: 0.7,
+
+  attack: () => {
+    if (Math.random() < myShip.accuracy) {
+      //damage accrued
+      alienShips.hull = alienShips.hull - myShip.firepower;
+      console.log(`Enemy ship has received ${myShip.firepower} damage.`);
+       if (alienShips.hull <= 0) {
+      console.log("Ship destroyed");
+    }
+    } else {
+      console.log("Attack missed");
+    }
+    console.log(Math.random(), myShip.accuracy, alienShips.hull);
+  },
+};
 
 class AlienShip {
-  constructor(hull, firepower, accuracy) {
+  constructor(hull, firepower, accuracy, hullLeft) {
     this.hull = hull;
     this.firepower = firepower;
     this.accuracy = accuracy;
@@ -42,27 +30,30 @@ class AlienShip {
     this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
   }
 
- attack(){
+  attack() {
     if (Math.random() < this.accuracy) {
+      //damage accrued
+      myShip.hull = myShip.hull - alienShips.firepower;
       console.log(`USS Medusa has received ${this.firepower} damage.`);
-    } else {
-        console.log('Enemy attack missed');
+      if (myShip.hull <= 0) {
+      console.log("Ship destroyed");
     }
-    console.log(Math.random(), this.accuracy);
+    }  else {
+      console.log("Enemy attack missed");
+    }
+    console.log(Math.random(), this.accuracy, myShip.hull);
   }
-};
+}
 
 let alienShips = new AlienShip();
-// const myShip = new MyShip();
 
+console.log(alienShips);
 console.log(myShip);
 console.log(alienShips.attack(myShip));
 console.log(myShip.attack(alienShips));
-console.log(alienShips.attack(myShip));
-console.log(myShip.attack(alienShips));
-console.log(alienShips.attack(myShip));
-console.log(myShip.attack(alienShips));
-console.log(alienShips);
+
 // const game = {
-    
+//     start: document.querySelector('button'),
+
+//     button.addEventListener('click', (event)=>{
 // }

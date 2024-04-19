e129238f263f8d1fae869c541c8712550cb3859b
Kiara Johnson <johns.kia35@gmail.com>
2024-04-19 17:05:09 -0600

    Test

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const myShip = {
   firepower: 5,
   accuracy: 0.7,
 
-  attack: () => {
+  attack: (alienShips) => {
     if (Math.random() < myShip.accuracy) {
       //damage accrued
-      alienShips.hull = alienShips.hull - myShip.firepower;
+      // console.log(alienShips[0].hull)
+   alienShips[0]['hull'] -= myShip.firepower;
+  //  console.log(alienShips[0]['hull']);
       console.log(`Enemy ship has received ${myShip.firepower} damage.`);
-      if (alienShips.hull <= 0) {
+      if (alienShips[0].hull <= 0) {
         console.log("Ship destroyed");
+        // ask user to continue playing or retreat
+        // attack first ship
+        // myShip.attack(alienShips[0]);
       }
     } else {
       console.log("Attack missed");
     }
-    console.log(Math.random(), myShip.accuracy, alienShips.hull);
+    console.log(Math.random(), myShip.accuracy, alienShips[0].hull);
   },
 };
 
@@ -30,10 +35,10 @@ class AlienShip {
     this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
   }
 
-  attack() {
+  attack(myShip) {
     if (Math.random() < this.accuracy) {
       //damage accrued
-      myShip.hull = myShip.hull - alienShips.firepower;
+      myShip.hull = myShip.hull - alienShips[0].firepower;
       console.log(`USS Medusa has received ${this.firepower} damage.`);
       if (myShip.hull <= 0) {
         console.log("Ship destroyed");
@@ -44,23 +49,43 @@ class AlienShip {
     console.log(Math.random(), this.accuracy, myShip.hull);
   }
 }
+let alienShips = [];
+for (let i = 0; i < 6; i++){  
+let alienArray = new AlienShip()
+  alienShips.push(alienArray)
+};
 
-let alienShips = new AlienShip();
 
-console.log(alienShips);
-console.log(myShip);
+// console.log(alienShips);
+// console.log(myShip);
 
-
+// const userImput = () => {
+//   confirm('Attack next ship?');
+// }
+const arrayShift = () => {
+  alienShips.shift();
+  // console.log(alienShips[0])
+}
 
 const start = () => {
   alert("Prepare for battle!");  
   //have my ship attack the alien ship
+  // console.log(alienShips);
+  // console.log(myShip);
   console.log(myShip.attack(alienShips));
-  //if alien ship not destroyed have alien ship attack
-  if (alienShips.hull >= 0) {
-    console.log(alienShips.attack(myShip));
+  //alien ship actions
+  if (alienShips[0].hull > 0) {
+    console.log(alienShips[0].attack(myShip));
+    if (alienShips[0].hull <= 0){
+  //       // alienShips.shift();
+        arrayShift();
+  //       // userImput();
+    }
   }
-};
+  //attack next ship?
 
+};
+// console.log(alienShips[0].hull);
 const button = document.querySelector("button");
 button.addEventListener("click", start);
+
